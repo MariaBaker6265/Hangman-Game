1cc39273153cdc31e4449166cd08514bd5aab63d
Maria Baker <maria.baker.bsee@outlook.com>
2017-11-04 00:12:51 -0400

    Hangman onclick event help needed.

---
+++ Assets/JavaScript/game.js
@@ -0,0 +1,14 @@
+
+<script type="text/javascript" src="Assets/JavaScript/game.js">
+
+
+var bands70s = ["Bee Gees", "Chic", "Queen", "Steve Miller", "Eagles"];
+
+function bandList(bandArray) {
+
+	// body...
+	for (var i = 0; i < bands70s.length; i++) {
+		console.log(bands70s[i]);
+	}
+	console.log("-------");
+}
